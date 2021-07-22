ccfb0b6545364548adbe5eac84ff94869f171ae3
liangzhirong <liangzhirong@yy.com>
2021-07-22 19:50:21 +0800

    chore: update

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,9 @@
 
 const path = require('path')
 const componentWithMDXScope = require('gatsby-mdx/component-with-mdx-scope')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const {returnMFConfig} = require('federation-exposes-plugin')
+const { dependencies } = require(path.resolve('./', 'package.json'))
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -60,3 +63,33 @@ exports.createPages = ({ graphql, actions }) => {
     )
   })
 }
+
+// exports.onCreateWebpackConfig = ({
+//   stage,
+//   rules,
+//   loaders,
+//   plugins,
+//   actions,
+// }) => {
+//   actions.setWebpackConfig({
+//     plugins: [
+//       // plugins.define({
+//       //   __DEV__: stage === `develop` || stage === `develop-html`,
+//       // }),
+//       new ModuleFederationPlugin(
+//         returnMFConfig({
+//           name: 'ge_components', // this will be used by the consuming federation host
+//           exposesOpts: {
+//             // paths to the components
+//             paths: ['./src/components/**/*.{t,j}s{,x}'],
+//             exclude: /\.?test\./,
+//             removePrefix: './src/components'
+//           },
+//           shared: {
+//             ...dependencies
+//           }
+//         })
+//       )
+//     ],
+//   })
+// }
